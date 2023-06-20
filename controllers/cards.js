897aa4f4868d9b920f481a5a63637059f7ae327c
Artem Sibisov <artsib7777@yandex.ru>
2023-06-21 03:32:40 +0800

    create controller and route for like/dislike card

--- controllers/cards.js
+++ controllers/cards.js
@@ -22,8 +22,30 @@ const deleteCard = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
+// PUT /cards/:cardId/likes — поставить лайк карточке
+const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  ).then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+// DELETE /cards/:cardId/likes — убрать лайк с карточки
+const dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  ).then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
 module.exports = {
   getCards,
   createCard,
   deleteCard,
+  likeCard,
+  dislikeCard,
 };
