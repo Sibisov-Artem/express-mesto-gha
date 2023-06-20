296cc9b1fef95dc5f894e0bfc21ea07c2e927ae6
Artem Sibisov <artsib7777@yandex.ru>
2023-06-20 18:03:00 +0800

    create models- userSchema and cardSchema

--- app.js
+++ app.js
@@ -10,23 +10,3 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.listen(PORT, () => {
   console.log(`Приложение слушает следующий порт: ${PORT}`);
 });
-
-// Зададим схему для пользователя через Mongoose:
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  about: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  avatar: {
-    type: String,
-    required: true,
-  },
-});
