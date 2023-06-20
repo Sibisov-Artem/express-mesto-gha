600cdf5814278ee61ef54825215c5ae3c529f316
Artem Sibisov <artsib7777@yandex.ru>
2023-06-20 19:53:51 +0800

    correct mongoose.connect for connected with db

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+// mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+}).then(() => {
+  console.log('connected to db');
+});
+
+app.get('/', (req, res) => {
+  res.send('проверка');
+});
 
 app.use(userRoutes);
 
