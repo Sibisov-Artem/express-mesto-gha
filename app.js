ed837c73c7140ab01f0c8184ed6def98d9c99f40
Artem Sibisov <artsib7777@yandex.ru>
2023-06-20 17:06:57 +0800

    create userSchema

--- app.js
+++ app.js
@@ -11,7 +11,22 @@ app.listen(PORT, () => {
   console.log(`Приложение слушает следующий порт: ${PORT}`);
 });
 
-/*В app.js подключитесь к серверу MongoDB по адресу:
-mongodb://localhost:27017/mestodb
-mestodb — имя базы данных, которая будет создана.*/
-
+// Зададим схему для пользователя через Mongoose:
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
