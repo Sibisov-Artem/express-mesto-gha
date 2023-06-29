f4f4ec04bb26e7c9a130ad90bb6f9c6ca57392bd
Artem Sibisov <artsib7777@yandex.ru>
2023-06-30 00:49:09 +0800

    hash password in controller user

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+
 const {
   ERROR_CODE,
   NOT_FOUND,
@@ -39,13 +41,14 @@ const createUser = (req, res) => {
     password,
   } = req.body;
 
-  User.create({
-    name,
-    about,
-    avatar,
-    email,
-    password,
-  })
+  bcrypt.hash(password, 10)
+    .then(hash => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
