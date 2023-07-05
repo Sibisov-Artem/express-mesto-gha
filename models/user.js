7cb933e67666eeeb98c4c9df91136a45c72ce426
Artem Sibisov <artsib7777@yandex.ru>
2023-07-05 21:17:48 +0800

    fix: after first review: add validation for avatar as URL in userSchema

--- models/user.js
+++ models/user.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: [validator.isURL, 'некорректный url'],
   },
   // email должен быть уникальным и валидироваться на соответствие схеме электронной почты.
   email: {
